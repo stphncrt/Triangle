2551dd5a6f7562e43ed5aaaac942204e0ea9c95e
stphncrt <cretestephen@gmail.com>
2020-11-12 13:34:25 +0300

    Files added

---
+++ script.js
@@ -0,0 +1,10 @@
+
+const first_side = document.getElementById('firstSide').nodeValue;
+const second_side = document.getElementById('secondSide').nodeValue;
+const third_side = document.getElementById('thirdSide').nodeValue;
+
+// deneme amaçlı. fonction değişecek
+function alan(first_side, second_side, third_side) = {
+    return(
+        first_side + second_side + third_side ;
+}
